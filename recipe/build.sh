46f433a05aff7b142fe11d4197830cf1c82e7e90
Isuru Fernando <isuruf@gmail.com>
2020-01-30 13:43:37 -0600

    Use cortex-a53 on aarch64

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,9 @@ if [[ "$target_platform" == "linux-ppc64le" ]]; then
   # HOST="powerpc64le-conda-linux-gnu" masks the fact that we are only
   # building for power8 and uses an older POWER architecture.
   GMP_HOST="power8-pc-linux-gnu"
+else if [[ "$target_platform" == "linux-aarch64" ]]; then
+  # Same here. Support Cortex-A53 and above only.
+  GMP_HOST="armcortexa53-pc-linux-gnu"
 else
   GMP_HOST=$HOST
 fi
