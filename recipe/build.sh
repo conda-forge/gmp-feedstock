547a7f05743641d8e66f38175f349a9596667de8
Isuru Fernando <isuruf@gmail.com>
2020-01-30 13:20:19 -0600

    Fix host for power8

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,16 @@
 
 chmod +x configure
 
-./configure --prefix=$PREFIX --enable-cxx --enable-fat --host=$HOST
+if [[ "$target_platform" == "linux-ppc64le" ]]; then
+  # HOST="powerpc64le-conda-linux-gnu" masks the fact that we are only
+  # building for power8 and uses an older POWER architecture.
+  GMP_HOST="power8-pc-linux-gnu"
+else
+  GMP_HOST=$HOST
+fi
 
-make
-make check
+./configure --prefix=$PREFIX --enable-cxx --enable-fat --host=$GMP_HOST
+
+make -j${CPU_COUNT}
+make check -j${CPU_COUNT}
 make install
