c0862b251374f82f17e9ec9a5ab1152cf1a6a534
Isuru Fernando <isuruf@gmail.com>
2020-09-02 15:41:32 -0500

    run autoreconf

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+cp $BUILD_PREFIX/share/libtool/build-aux/config.guess config.fsf.guess
+cp $BUILD_PREFIX/share/libtool/build-aux/config.sub config.fsf.sub
 
 shopt -s extglob
 chmod +x configure
+
+if [[ "$target_platform" == osx-arm64 ]]; then
+  ./autoreconf -vfi
+fi
 
 mkdir build
 cd build
