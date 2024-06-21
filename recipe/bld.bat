bba1c4759776e6ff9e55306fa64147b78e6cd640
Isuru Fernando <isuruf@gmail.com>
2024-06-20 19:32:44 -0500

    Fix CONDA_PREFIX

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 echo source %SYS_PREFIX:\=/%/etc/profile.d/conda.sh    > conda_build.sh
-echo env                                              >> conda_build.sh
-echo conda activate "${BUILD_PREFIX}"                 >> conda_build.sh
-echo conda activate --stack "${PREFIX}"               >> conda_build.sh
+echo conda activate "${PREFIX}"                       >> conda_build.sh
+echo conda activate --stack "${BUILD_PREFIX}"         >> conda_build.sh
+echo CONDA_PREFIX=${CONDA_PREFIX//\\//}               >> conda_build.sh
 type "%RECIPE_DIR%\build.sh"                          >> conda_build.sh
 
 set PREFIX=%PREFIX:\=/%
